82e877a42d86aaa8fd2a8783161f05d45e5ad936
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-24 07:30:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,4 @@
-def def roll_call_dwarves(array)
+def roll_call_dwarves(array)
   i = 0 
   while i < array.length 
     puts "#{i + 1}. #{array[i]}"
@@ -9,7 +9,7 @@ end
 roll_call_dwarves(dwarves)
 end
 
-def def summon_captain_planet(array)
+def summon_captain_planet(array)
   nu_array = []
   i = 0 
   while i < array.length 
@@ -22,8 +22,17 @@ end
 summon_captain_planet(planeteer_calls)
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array) 
+  i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
+end
+
+long_planeteer_calls(call_screams)
 end
 
 def find_the_cheese# code an argument here
