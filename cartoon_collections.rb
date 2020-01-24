cdeffb6cb0adce9050c0898373772b28aae8189c
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-24 07:42:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,17 +11,10 @@ def summon_captain_planet(array)
   planeteer_calls
 end
 
-def long_planeteer_calls(array) 
-  i = 0 
-  if  array.any? {|i| i.length > 4}
-    return true
-  else 
-    return false
-  i = i + 1
+def long_planeteer_calls(array)
+  array.any? do |word|
+    word.length > 4
   end 
-end
-
-long_planeteer_calls(call_screams)
 end
 
 def find_the_cheese(array)
